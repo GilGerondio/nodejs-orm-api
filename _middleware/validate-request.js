a9164b260732079c884de81fdab21ac725f0a3d4
Gilbert Gerondio <issagilbert0909@gmail.com>
2024-03-06 14:40:25 +0800

    first commit

---
+++ _middleware/validate-request.js
@@ -0,0 +1,18 @@
+const { mapValueFieldNames } = require("sequelize/types/utils");
+
+module.exports = validateRequest;
+
+function validateRequest(req, next, schema) {
+    const options = {
+        abortEarly: false, // include all errors
+        allowUnkown: true, // ignore all unkown props
+        stripUnkown: true // remove unkown props
+    };
+    const { error, value } = schema.validate(req.body, options);
+    if (error) {
+        next('Validation error: ${error.details.map(x => x.message).join(', ')}');   
+    } else {
+        req.body = value;
+        next();
+    }
+}
